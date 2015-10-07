103beaddff946d3280dbd9e8c47bd44b7aad8fea
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-10-07 08:25:59 -0700

    Skeleton implementation for Sync and Async Operation Runners

--- src/main/java/sir/wellington/alchemy/http/OperationRunner.java
+++ src/main/java/sir/wellington/alchemy/http/OperationRunner.java
@@ -18,7 +18,11 @@ package sir.wellington.alchemy.http;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import org.apache.http.client.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sir.wellington.alchemy.http.exceptions.HttpException;
 import sir.wellington.alchemy.http.operations.HttpOperation;
+import sir.wellington.alchemy.http.operations.HttpRequest;
 import sir.wellington.alchemy.http.operations.HttpVerb;
 
 /**
@@ -31,35 +35,119 @@ interface OperationRunner<ResponseType> extends Callable<ResponseType>
     @Override
     public ResponseType call() throws Exception;
 
-    
     class Sync<ResponseType> implements OperationRunner<ResponseType>
     {
-        private HttpClient httpClient;
+
+        private static final Logger LOG = LoggerFactory.getLogger(Sync.class);
+
+        private HttpRequest request;
+        private HttpClient apacheHttpClient;
         private HttpVerb verb;
         Class<ResponseType> classOfResponseType;
-        
+
         @Override
         public ResponseType call() throws Exception
         {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            LOG.debug("Running HTTP Request {}", request);
+
+            HttpResponse response = null;
+
+            try
+            {
+                response = verb.execute(apacheHttpClient, request);
+            }
+            catch (HttpException ex)
+            {
+                LOG.error("Encountered HttpException when running verb {} on request {}", verb, request, ex);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                LOG.error("Failed to execute verb {} on request {}", verb, request, ex);
+                throw ex;
+            }
+
+            if (response == null)
+            {
+                LOG.error("HTTP Verb {} returned null response", verb);
+                throw new HttpException("HTTP Verb returned null response");
+            }
+
+            LOG.debug("HTTP Request {} successfully executed: {}", request, response);
+            if (clientWants(HttpResponse.class))
+            {
+                return (ResponseType) response;
+            }
+            else if (clientWants(String.class))
+            {
+                return (ResponseType) response.asString();
+            }
+            else
+            {
+                return response.as(classOfResponseType);
+            }
         }
-        
+
+        private boolean clientWants(Class<?> aClass)
+        {
+            return this.classOfResponseType == aClass;
+        }
+
     }
-    
+
     class Async<ResponseType> implements OperationRunner<Void>
     {
-        private HttpClient httpClient;
-        private HttpVerb verb;
-        private Class<ResponseType> classOfResponseType;
+
+        private static final Logger LOG = LoggerFactory.getLogger(Async.class);
+
+        private OperationRunner<ResponseType> synchronousDelegate;
+
         private ExecutorService async;
         private HttpOperation.OnSuccess<ResponseType> successCallback;
         private HttpOperation.OnFailure failureCallback;
-        
+
         @Override
         public Void call() throws Exception
         {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            async.submit(this::run);
+
+            return null;
         }
-        
+
+        private void run()
+        {
+            LOG.debug("Starting Async HTTP Request using delegate {}", synchronousDelegate);
+
+            ResponseType response;
+
+            try
+            {
+                response = synchronousDelegate.call();
+            }
+            catch (HttpException ex)
+            {
+                LOG.error("Http Operation Failed", ex);
+                failureCallback.handleError(ex);
+                return;
+            }
+            catch (Exception ex)
+            {
+                LOG.error("Async request failed", ex);
+                failureCallback.handleError(new HttpException(ex));
+                return;
+            }
+
+            try
+            {
+                successCallback.processResponse(response);
+            }
+            catch (Exception ex)
+            {
+                String message = "Success Callback threw exception";
+                LOG.warn(message, ex);
+                failureCallback.handleError(new HttpException(message, ex));
+            }
+        }
+
     }
 }
