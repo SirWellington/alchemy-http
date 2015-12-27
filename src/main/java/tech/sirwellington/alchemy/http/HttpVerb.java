babc038f29b565a095dd8fd743f83885cbd2baf7
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-27 09:24:08 -0800

    HttpVerb updates

--- src/main/java/tech/sirwellington/alchemy/http/HttpVerb.java
+++ src/main/java/tech/sirwellington/alchemy/http/HttpVerb.java
@@ -17,12 +17,16 @@ package tech.sirwellington.alchemy.http;
 
 import org.apache.http.client.HttpClient;
 import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern;
 import tech.sirwellington.alchemy.http.exceptions.AlchemyHttpException;
+
+import static tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.INTERFACE;
 
 /**
  *
  * @author SirWellington
  */
+@StrategyPattern(role = INTERFACE)
 @Internal
 public interface HttpVerb
 {
