9f76400d2b65422735ecf5d347226a687241f8bf
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-02 10:13:37 -0800

    Updates error message

--- src/main/java/tech/sirwellington/alchemy/http/AlchemyMachineImpl.java
+++ src/main/java/tech/sirwellington/alchemy/http/AlchemyMachineImpl.java
@@ -26,7 +26,6 @@ import tech.sirwellington.alchemy.http.AlchemyRequest.OnFailure;
 import tech.sirwellington.alchemy.http.AlchemyRequest.OnSuccess;
 import tech.sirwellington.alchemy.http.exceptions.AlchemyHttpException;
 
-import static java.lang.String.format;
 import static tech.sirwellington.alchemy.annotations.designs.StepMachineDesign.Role.MACHINE;
 import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
@@ -158,14 +157,12 @@ final class AlchemyMachineImpl implements AlchemyHttpStateMachine
         checkThat(response)
                 .throwing(ex -> new AlchemyHttpException(request, "HTTP Verb returned null response"))
                 .is(notNull());
-
+        
         checkThat(response)
-                .throwing(ex -> new AlchemyHttpException(request,
-                                                        response,
-                                                        format("Http Response not OK. Status Code: %d Body: %s", 
-                                                               response.statusCode(),
-                                                               response.bodyAsString())))
-                .is(okResponse());
+            .throwing(ex -> new AlchemyHttpException(request,
+                                                     response,
+                                                     "Http Response not OK. Status Code: " + response.statusCode()))
+            .is(okResponse());
 
         LOG.debug("HTTP Request {} successfully executed: {}", request, response);
 
