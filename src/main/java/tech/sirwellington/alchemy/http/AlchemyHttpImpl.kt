7cb9556c03dd941c7145caefd661be4ff1637252
Wellington Moreno <wellington@censio.love>
2018-01-11 17:39:39 -0800

    Rewrites AlchemyHttpImpl in kotlin

---
+++ src/main/java/tech/sirwellington/alchemy/http/AlchemyHttpImpl.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2018. Sir Wellington.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.sirwellington.alchemy.http
+
+import sir.wellington.alchemy.collections.maps.Maps
+import tech.sirwellington.alchemy.annotations.access.Internal
+import tech.sirwellington.alchemy.annotations.concurrency.ThreadSafe
+import tech.sirwellington.alchemy.annotations.designs.patterns.BuilderPattern
+import tech.sirwellington.alchemy.annotations.designs.patterns.BuilderPattern.Role.PRODUCT
+import tech.sirwellington.alchemy.arguments.Arguments.checkThat
+import tech.sirwellington.alchemy.arguments.assertions.nonEmptyString
+
+/**
+ *
+ * @author SirWellington
+ */
+@Internal
+@BuilderPattern(role = PRODUCT)
+@ThreadSafe
+internal class AlchemyHttpImpl(private val defaultHeaders: Map<String, String>,
+                               private val stateMachine: AlchemyHttpStateMachine) : AlchemyHttp
+{
+
+
+    override fun usingDefaultHeader(key: String, value: String): AlchemyHttp
+    {
+        checkThat(key)
+                .usingMessage("Key is empty")
+                .isA(nonEmptyString())
+
+        val copy = Maps.mutableCopyOf(defaultHeaders)
+        copy[key] = value
+        return AlchemyHttpImpl(defaultHeaders = copy, stateMachine = stateMachine)
+    }
+
+    override fun go(): AlchemyRequest.Step1
+    {
+        val initialRequest = HttpRequest.Builder
+                                        .newInstance()
+                                        .usingRequestHeaders(defaultHeaders)
+                                        .build()
+
+        return stateMachine.begin(initialRequest)
+    }
+
+    override fun toString(): String
+    {
+        return "AlchemyHttp{defaultHeaders=$defaultHeaders, stateMachine=$stateMachine}"
+    }
+
+    override fun getDefaultHeaders(): Map<String, String>
+    {
+        //Already immutable
+        return defaultHeaders
+    }
+
+}
